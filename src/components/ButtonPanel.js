6acbb62c4c643b24e3bb4a2f67a6a39359919077
Daniel <danieldrv8@gmail.com>
2024-01-09 20:59:27 +0100

    mod buttonpanel

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -12,29 +12,27 @@ const ButtonPanel = (props) => {
           <button onClick={() => handleClick("+/-")}>+/-</button>
           <button onClick={() => handleClick("%")}>%</button>
           <button onClick={() => handleClick("/")}>÷</button>
-        </div>
-        <div>
-          <button onClick={() => handleClick("7")}>7</button>
-          <button onClick={() => handleClick("8")}>8</button>
-          <button onClick={() => handleClick("9")}>9</button>
-          <button onClick={() => handleClick("*")}>x</button>
-        </div>
-        <div>
-          <button onClick={() => handleClick("4")}>4</button>
-          <button onClick={() => handleClick("5")}>5</button>
-          <button onClick={() => handleClick("6")}>6</button>
-          <button onClick={() => handleClick("-")}>-</button>
+          <button onClick={() => handleClick("=")}>=</button>
         </div>
         <div>
           <button onClick={() => handleClick("1")}>1</button>
-          <button onClick={() => handleClick("2")}>2</button>
-          <button onClick={() => handleClick("3")}>3</button>
-          <button onClick={() => handleClick("+")}>+</button>
+          <button onClick={() => handleClick("4")}>4</button>
+          <button onClick={() => handleClick("7")}>7</button>
+          <button onClick={() => handleClick("*")}>x</button>
+          <button onClick={() => handleClick(".")}>.</button>
         </div>
         <div>
+          <button onClick={() => handleClick("2")}>2</button>
+          <button onClick={() => handleClick("5")}>5</button>
+          <button onClick={() => handleClick("8")}>8</button>
+          <button onClick={() => handleClick("-")}>-</button>
           <button onClick={() => handleClick("0")}>0</button>
-          <button onClick={() => handleClick(".")}>.</button>
-          <button onClick={() => handleClick("=")}>=</button>
+        </div>
+        <div>
+          <button onClick={() => handleClick("3")}>3</button>
+          <button onClick={() => handleClick("6")}>6</button>
+          <button onClick={() => handleClick("9")}>9</button>
+          <button onClick={() => handleClick("+")}>+</button>
         </div>
       </div>
     );
